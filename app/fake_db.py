d0baf8f6766e1f3691d31f5d53abfd38b98d766d
kavar <kavar@ka.com>
2023-04-15 20:46:55 +0400

    add fake data DB

---
+++ app/fake_db.py
@@ -0,0 +1,14 @@
+
+class Fake_DB:
+    fake_users = [
+        {"id":1, "name": "ilya"},
+        {"id":2, "name": "ilya2"},
+        {"id":3, "name": "ilya3", "info": [{"sex":"m", "old": 44}]},
+    ]
+
+    fake_data = [
+        {"id": 1, "count": 5, "money": 100},
+        {"id": 2, "count": 15, "money": 500},
+        {"id": 3, "count": 35, "money": 1600},
+        {"id": 4, "count": 54, "money": 10430},
+    ]
